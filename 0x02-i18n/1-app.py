84cc03537da3447a1e1679c699f100d682b92ce4
JustusOmbok <ombokjustus@gmail.com>
2024-02-06 15:51:04 +0300

    babel use

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Module for flask app.
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+# Instantiate the Flask app
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+# Instantiate the Babel object
+babel = Babel(app)
+
+# Config class for configuring available languages
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+# Use Config as config for the Flask app
+app.config.from_object(Config)
+
+@app.route('/')
+def index():
+    """Define index function."""
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
