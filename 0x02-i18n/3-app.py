46d96efd2792db9217e64508cb85983c14d8ee68
JustusOmbok <ombokjustus@gmail.com>
2024-02-06 17:29:31 +0300

    languages

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -11,11 +11,14 @@ app.url_map.strict_slashes = False
 # Instantiate the Babel object
 babel = Babel(app)
 
-# Config class for configuring available languages
+
 class Config:
+    """Config class for configuring available languages.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 # Use Config as config for the Flask app
 app.config.from_object(Config)
@@ -26,10 +29,12 @@ def get_locale():
     """Determine the best match for supported languages."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
     """Define index function."""
-    return render_template('3-index.html', title=_('home_title'), header=_('home_header'))
+    return render_template('3-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
