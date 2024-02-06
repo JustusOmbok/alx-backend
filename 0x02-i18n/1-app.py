8d992f1eef68456a7e55d0fa3aaffc568aaa5dc9
JustusOmbok <ombokjustus@gmail.com>
2024-02-06 15:57:15 +0300

    babel use

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -11,19 +11,23 @@ app.url_map.strict_slashes = False
 # Instantiate the Babel object
 babel = Babel(app)
 
+
 # Config class for configuring available languages
 class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 # Use Config as config for the Flask app
 app.config.from_object(Config)
+
 
 @app.route('/')
 def index():
     """Define index function."""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
