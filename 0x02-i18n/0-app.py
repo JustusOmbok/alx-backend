1baede893bd16a5dbb59c41a2ae7b5d3850071ad
JustusOmbok <ombokjustus@gmail.com>
2024-02-06 15:28:32 +0300

    localization

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Welcome to Holberton')
+    return render_template('0-index.html', title='Welcome to Holberton')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
