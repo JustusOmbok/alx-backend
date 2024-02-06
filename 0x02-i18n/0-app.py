a085459bf74ff398b14f14e6fb38be39c14741c3
JustusOmbok <ombokjustus@gmail.com>
2024-02-06 15:36:28 +0300

    localization

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
+"""Module for flask app.
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
 def index():
+    """Define index function."""
     return render_template('0-index.html', title='Welcome to Holberton')
 
 
