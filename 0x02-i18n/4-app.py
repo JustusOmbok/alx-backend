17921988c09d5796685895129b8c8d6994e1f446
JustusOmbok <ombokjustus@gmail.com>
2024-02-06 18:32:21 +0300

    languages

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Module for flask app.
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+# Instantiate the Flask app
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+# Instantiate the Babel object
+babel = Babel(app)
+
+
+class Config:
+    """Config class for configuring available languages.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Use Config as config for the Flask app
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match for supported languages."""
+    # Check if the 'locale' parameter is present in the request arguments
+    if 'locale' in request.args:
+        # Get the value of the 'locale' parameter
+        requested_locale = request.args.get('locale')
+        # Check if the requested locale is in the list of supported languages
+        if requested_locale in app.config['LANGUAGES']:
+            return requested_locale
+
+    # If the 'locale' parameter is not present or not supported,
+    # use default behavior
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Define index function."""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
